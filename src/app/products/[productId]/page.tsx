8c3f7c2d3c4272258f50224d5464d62b8acdd470
Legaz <worldoflegaz@icloud.com>
2025-06-22 21:19:09 +0900

    routing metadata,title metdata, link component, active links, params and searchParams.

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,4 +1,21 @@
-export default async function ProductDetails({params,}: {params: Promise<{ productId: string}>;}){
-    const productId = (await params).productId;
-    return <h1>Details about product {productId}</h1>
- }
+import { Metadata } from "next";
+
+type Props = {
+  params: Promise<{ productId: string }>;
+};
+export const generateMetadata = async ({params}: Props): Promise<Metadata> => {
+    const id = (await params).productId;
+    const title = await new Promise((reslove)=>{
+        setTimeout(()=>{
+            reslove(`iPhone${id}`);
+        },100)
+    })
+    return {
+        title: `Product ${title}`
+    };
+};
+
+export default async function ProductDetails({ params }: Props) {
+  const productId = (await params).productId;
+  return <h1>Details about product {productId}</h1>;
+}
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,18 @@
+import Link from "next/link"
+
+
 export default function Products() {
     return <>
     <ul>
+        <Link href={"/"}>Home</Link>
         <li>
-            First product
+            <Link href={"/products/1"}>First product</Link>
         </li>
         <li>
-            Second product
+            <Link href={"/products/2"}>Second product</Link>
         </li>
         <li>
-            Third product
+            <Link href={"/products/3"} replace>Third product</Link>
         </li>
     </ul>
         </>
